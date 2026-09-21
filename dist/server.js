f450f88c59add79efcf53a4c4dbc37a3c66ef60a
goga0917 <goga0917@gmail.com>
2026-09-22 06:08:19 +0900

    BRR: Initial Setup

---
+++ dist/server.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Executed!");
+const person = "Goga";
+const sount = 200;
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+console.log("Executed!")
+
+import moment from "moment";
+
+const person: string = "Goga";
+const sount: number = 200;
