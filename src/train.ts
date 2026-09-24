f2f6c58b9e0543d1e19d06d1705c039c63cbc4b4
goga0917 <goga0917@gmail.com>
2026-09-24 15:41:49 +0900

    update train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,16 @@
 console.log("Training model...");
+// M-Task
+
+interface SquareResult {
+  number: number;
+  square: number;
+}
+
+function getSquareNumbers(numbers: number[]): SquareResult[] {
+  return numbers.map((num: number): SquareResult => ({
+    number: num,
+    square: num * num
+  }));
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
