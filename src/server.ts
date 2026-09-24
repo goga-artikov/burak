807842f59cfae26ae2beb0657218597f59187254
goga0917 <goga0917@gmail.com>
2026-09-24 15:29:55 +0900

    feat: express integration

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGODB_URL as string, {})
   .then((data) => {
-      console.log("MongoDB connected");
-      const PORT = process.env.PORT ?? 3003;
+    console.log("MongoDB connected");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`The server is running succesfully on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
-
-
