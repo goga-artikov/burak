589c3efc1fe5184ebb39f878434e565acb5a858d
goga0917 <goga0917@gmail.com>
2026-09-26 22:48:36 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
 import express from "express";
 import path from "path";
-import router from "./routers";
+import router from "./router";
+import routerAdmin from "./routerAdmin";
+
 
 /** 1- ENTERANSE  **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 /** 2- SESSIONS **/
 
 /** 3- VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
 /** 4- ROUTES **/
-app.use('/', router); //middleware design pattern
-
+app.use("/admin", routerAdmin); // // SSR: EJS
+app.use("/", router); // SPA: REACT
 
 export default app;
